121ef4c25da9322497be1a2956e4c19b6129b8a2
mac <mac@notarealemail.com>
2019-10-30 08:14:25 -0400

    added bidirectional communication

--- CommunicationTest WatchKit Extension/InterfaceController.swift
+++ CommunicationTest WatchKit Extension/InterfaceController.swift
@@ -8,19 +8,55 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
+
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+
+    
+    // MARK: Outlets
+    // ---------------------
+    @IBOutlet var messageLabel: WKInterfaceLabel!
+    
+    
+    // MARK: Delegate functions
+    // ---------------------
+
+    // Default function, required by the WCSessionDelegate on the Watch side
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //@TODO
+    }
+    
+    // 3. Get messages from PHONE
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        print("WATCH: Got message from Phone")
+        // Message from phone comes in this format: ["course":"MADT"]
+        let messageBody = message["course"] as! String
+        messageLabel.setText(messageBody)
+    }
+    
 
 
-class InterfaceController: WKInterfaceController {
-
+    
+    // MARK: WatchKit Interface Controller Functions
+    // ----------------------------------
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        
+        // 1. Check if teh watch supports sessions
+        if WCSession.isSupported() {
+            WCSession.default.delegate = self
+            WCSession.default.activate()
+        }
+        
+        
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        
     }
     
     override func didDeactivate() {
@@ -28,4 +64,33 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    
+    // MARK: Actions
+    // ---------------------
+    
+    // 2. When person presses button on watch, send a message to the phone
+    @IBAction func buttonPressed() {
+        
+        if WCSession.default.isReachable {
+            print("Attempting to send message to phone")
+            self.messageLabel.setText("Sending msg to watch")
+            WCSession.default.sendMessage(
+                ["name" : "Pritesh"],
+                replyHandler: {
+                    (_ replyMessage: [String: Any]) in
+                    // @TODO: Put some stuff in here to handle any responses from the PHONE
+                    print("Message sent, put something here if u are expecting a reply from the phone")
+                    self.messageLabel.setText("Got reply from phone")
+            }, errorHandler: { (error) in
+                //@TODO: What do if you get an error
+                print("Error while sending message: \(error)")
+                self.messageLabel.setText("Error sending message")
+            })
+        }
+        else {
+            print("Phone is not reachable")
+            self.messageLabel.setText("Cannot reach phone")
+        }
+    }
+    
 }
